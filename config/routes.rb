7582f11ebe001bd90ac6f89112cc52879c6e8dd3
Arish <thermic.arish@gmail.com>
2024-03-10 18:15:08 +0530

    Complete devise views

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,15 @@
 
 Rails.application.routes.draw do
   get 'dashboard', to: 'dashboard#index', as: 'dashboard'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    confirmations: 'users/confirmations'
+  }
+  devise_scope :user do
+    get '/confirmation_sent', to: 'users/confirmations#confirmation_sent', as: 'confirmation_sent'
+  end
   root 'home#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
