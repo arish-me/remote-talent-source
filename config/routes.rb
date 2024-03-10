9192b9d092119073b8a96c2edc407ee1864a3da5
Arish <thermic.arish@gmail.com>
2024-03-10 16:11:03 +0530

    Added Devise Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'dashboard', to: 'dashboard#index', as: 'dashboard'
   devise_for :users
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
