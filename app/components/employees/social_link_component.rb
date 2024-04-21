bce6e49a19b7011184d9c6262551ea80fe6d3ba0
Arish <thermic.arish@gmail.com>
2024-04-21 11:43:33 +0530

    Added Social profile links

---
+++ app/components/employees/social_link_component.rb
@@ -0,0 +1,37 @@
+module Employees
+class SocialLinkComponent < ApplicationComponent
+  include LinksHelper
+
+  attr_reader :network
+
+  def initialize(handle, network)
+    @handle = handle
+    @network = network
+  end
+
+  def render?
+    handle.present?
+  end
+
+  def handle
+    sanitize(@handle)
+  end
+
+  def url
+    case network
+    when :github
+      "https://github.com/#{handle}"
+    when :twitter
+      "https://twitter.com/#{handle}"
+    when :linkedin
+      "https://www.linkedin.com/in/#{handle}"
+    when :stack_overflow
+      "https://stackoverflow.com/users/#{handle}"
+    end
+  end
+
+  def icon
+    "icons/brands/#{network}.svg"
+  end
+end
+end
