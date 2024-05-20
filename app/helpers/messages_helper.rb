c8a940ef23096c2c50180707db2f7691b2b349fd
Arish <thermic.arish@gmail.com>
2024-05-20 21:29:26 +0530

    More Enhancement

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 module MessagesHelper
+  def display_time(time)
+    time_diff = Time.now - time
+
+    if time_diff < 1.minute
+      "a few seconds ago"
+    elsif time.to_date == Date.today
+      "Today at #{time.strftime('%I:%M %p')}"
+    elsif time.to_date == Date.yesterday
+      "Yesterday at #{time.strftime('%I:%M %p')}"
+    else
+      time.strftime('%b %d, %Y at %I:%M %p')
+    end
+  end
 end
